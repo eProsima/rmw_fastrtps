9af6a1eddeaefbc9d99a8f7203589cef09cb3d6c
Ricardo González Moreno <ricardo@richiware.dev>
2022-06-24 11:15:28 +0200

    Initial commit
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- rmw_fastrtps_cpp/src/rmw_client.cpp
+++ rmw_fastrtps_cpp/src/rmw_client.cpp
@@ -190,7 +190,6 @@ rmw_create_client(
   auto cleanup_info = rcpputils::make_scope_exit(
     [info, dds_participant]() {
       delete info->pub_listener_;
-      delete info->listener_;
       if (info->response_type_support_) {
         dds_participant->unregister_type(info->response_type_support_.get_type_name());
       }
@@ -241,13 +240,6 @@ rmw_create_client(
   info->response_type_support_ = response_fastdds_type;
 
   /////
-  // Create Listeners
-  info->listener_ = new (std::nothrow) ClientListener(info);
-  if (!info->listener_) {
-    RMW_SET_ERROR_MSG("create_client() failed to create response subscriber listener");
-    return nullptr;
-  }
-
   info->pub_listener_ = new (std::nothrow) ClientPubListener(info);
   if (!info->pub_listener_) {
     RMW_SET_ERROR_MSG("create_client() failed to create request publisher listener");
@@ -323,8 +315,7 @@ rmw_create_client(
   // Creates DataReader
   info->response_reader_ = subscriber->create_datareader(
     response_topic_desc,
-    reader_qos,
-    info->listener_);
+    reader_qos);
 
   if (!info->response_reader_) {
     RMW_SET_ERROR_MSG("create_client() failed to create response DataReader");
